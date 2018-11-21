a828c6b0aa1c1e1ab0d8dd32f0db0d203fc87f04
Brian James <brianb.james@mac.com>
2018-11-20 19:08:19 -0800

    Lets Go Live

---
+++ navbarInject.js
@@ -0,0 +1,21 @@
+document.write("<div class=\"container\">");
+document.write("    <div class=\"navbar-header\">");
+document.write("      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">");
+document.write("        <span class=\"sr-only\">Toggle navigation<\/span>");
+document.write("        <span class=\"icon-bar\"><\/span>");
+document.write("        <span class=\"icon-bar\"><\/span>");
+document.write("        <span class=\"icon-bar\"><\/span>");
+document.write("      <\/button>");
+document.write("      <a class=\"navbar-brand\" href=\"http:\/\/lefthandpathwax.net\">Left Hand Path<\/a>");
+document.write("    <\/div>");
+document.write("    <div id=\"navbar\" class=\"navbar-collapse collapse\">");
+document.write("      <ul class=\"nav navbar-nav\">");
+document.write("		<li><a href=\"http:\/\/twitter.com\/left_hand_path\" target=\"_blank\">Twitter<\/a><\/li>");
+document.write("		<li><a href=\"http:\/\/facebook.com\/lefthandpathwax\" target=\"_blank\">Facebook<\/a><\/li>");
+document.write("		<li><a href=\"http:\/\/soundcloud.com\/lefthandpathwax\" target=\"_blank\">Soundcloud<\/a><\/li>");  
+document.write("		<li><a href=\"http:\/\/lefthandpathwax.bandcamp.com\" target=\"_blank\">Bandcamp<\/a><\/li>");
+document.write("		<li><a href=\"mailto:lefthandpathwax@gmail.com\">Contact<\/a><\/li><\/ul>");  
+document.write("    <\/div><!--\/.nav-collapse -->");
+document.write("  <\/div>");
+                                                                          
+
